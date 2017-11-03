ff58c6d877622678ebab4e27cb5b37b9cd5d9c52
Jo-ravar <ravijoshi0996@gmail.com>
2017-11-03 23:28:16 +0530

    day 1 code

---
+++ Day1/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('port', process.env.PORT || 3000);
+
+app.get('/user', function(req, res) {
+  const responseObject = {
+    name: 'sakshi',
+    age: 25,
+  };
+  res.send(responseObject);
+});
+
+app.post('/login', function(req, res) {
+  const userName = req.body.username;
+  const password = req.body.password;
+  res.send('Your username was ' + userName + ' and password was ' + password);
+});
+
+app.listen(app.get('port'), function(err) {
+  if (!err) {
+    console.log('server started at port 3000');
+  }
+});
